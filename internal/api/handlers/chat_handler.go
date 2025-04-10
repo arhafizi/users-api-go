729b59813aaeb272e187402985ec29b42bf0a02f
arh <a.hafizi9@outlook.com>
2025-04-10 14:00:06 +0330

    cache messages history

--- internal/api/handlers/chat_handler.go
+++ internal/api/handlers/chat_handler.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"fmt"
+	"log/slog"
 	"net/http"
 	"strconv"
+	"time"
 
 	"example.com/api/internal/api/responses"
+	dbCtx "example.com/api/internal/repository/db"
 	"example.com/api/internal/services"
 	"example.com/api/internal/services/chat"
 	"example.com/api/pkg/logging"
@@ -65,11 +69,31 @@ func (h *ChatHandler) GetMessageHistory(c *gin.Context) {
 	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "50"))
 	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "0"))
 
-	messages, err := h.service.Chat().GetMessages(c.Request.Context(), int32(limit), int32(offset))
+	cacheKey := fmt.Sprintf("messages:limit:%d:offset:%d", limit, offset)
+
+	var messages []dbCtx.GetMessagesRow
+	found, err := h.service.CacheStorage().Get(c.Request.Context(), cacheKey, &messages)
+	if err != nil {
+		slog.Error("cache", "redis", "Failed to get cache")
+		responses.InternalServerError(c, "Failed to fetch message history")
+		return
+	}
+
+	if found {
+		responses.OK(c, "Message history retrieved from cache", messages)
+		return
+	}
+
+	messages, err = h.service.Chat().GetMessages(c.Request.Context(), int32(limit), int32(offset))
 	if err != nil {
 		responses.InternalServerError(c, "Failed to fetch message history")
 		return
 	}
 
+	if err := h.service.CacheStorage().Set(c.Request.Context(), cacheKey, messages, 5*time.Minute); err != nil {
+		slog.Error("cache", "redis", "Failed to SET cache")
+		responses.InternalServerError(c, "Failed to cache message history")
+		return
+	}
 	responses.OK(c, "Message history retrieved successfully", messages)
 }
