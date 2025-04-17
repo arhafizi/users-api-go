9174fce99a62ff2a8594f4bd6968444f36c29a1c
arh <a.hafizi9@outlook.com>
2025-04-17 11:34:12 +0330

    use simple gin in-memory cache

--- internal/api/routes/user_routes.go
+++ internal/api/routes/user_routes.go
@@ -1,14 +1,21 @@
 package routes
 
 import (
+	"time"
+
 	"example.com/api/internal/api/handlers"
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 )
+
+var store = persistence.NewInMemoryStore(time.Second)
 
 func SetupUserRoutes(router *gin.RouterGroup, h *handlers.UserHandler) {
 	users := router.Group("/users")
 	{
 		users.GET("", h.GetAll)
+		users.GET("/cached", cache.CachePage(store, time.Minute, h.GetAll))
 		users.GET("/:id", h.GetByID)
 		users.POST("", h.Create)
 		users.PUT("/:id", h.UpdateFull)
